8974b0a4e6be06a981f7b7cdb89bacf60f3fb27f
shinya.sugiyama <shinya.sugiyama@locondo.co.jp>
2019-08-07 18:08:22 +0900

    Update Convert Script

---
+++ check_current_mysql_session_without_daemon.sql
@@ -0,0 +1,36 @@
+SELECT pps.thread_id AS thd_id,
+       pps.processlist_id AS conn_id,
+       IF(pps.name = 'thread/sql/one_connection',
+          CONCAT(pps.processlist_user, '@', pps.processlist_host),
+          REPLACE(pps.name, 'thread/', '')) user,
+       pps.processlist_db AS db,
+       pps.processlist_command AS command,
+       pps.processlist_state AS state,
+       pps.processlist_time AS time,
+       sys.format_statement(pps.processlist_info) AS current_statement,
+       IF(esc.end_event_id IS NULL,
+          sys.format_time(esc.timer_wait),
+          NULL) AS statement_latency,
+       sys.format_time(esc.lock_time) AS lock_latency,
+       esc.rows_examined AS rows_examined,
+       esc.rows_sent AS rows_sent,
+       esc.rows_affected AS rows_affected,
+       esc.created_tmp_tables AS tmp_tables,
+       esc.created_tmp_disk_tables AS tmp_disk_tables,
+       IF(esc.no_good_index_used > 0 OR esc.no_index_used > 0, 'YES', 'NO') AS full_scan,
+       IF(esc.end_event_id IS NOT NULL,
+          sys.format_statement(esc.sql_text),
+          NULL) AS last_statement,
+       IF(esc.end_event_id IS NOT NULL,
+          sys.format_time(esc.timer_wait),
+          NULL) AS last_statement_latency,
+       ewc.event_name AS last_wait,
+       IF(ewc.end_event_id IS NULL AND ewc.event_name IS NOT NULL,
+          'Still Waiting',
+          sys.format_time(ewc.timer_wait)) last_wait_latency,
+       ewc.source
+  FROM performance_schema.threads AS pps
+  LEFT JOIN performance_schema.events_waits_current AS ewc USING (thread_id)
+  LEFT JOIN performance_schema.events_statements_current as esc USING (thread_id)
+  where pps.processlist_id IS NOT NULL AND pps.processlist_command != 'Daemon'
+  ORDER BY pps.processlist_time DESC, last_wait_latency DESC;
