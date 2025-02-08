f1f290832091f97ad13b7dee99e2de622c14d40f
SHINYA <shinya.s.jpn@gmail.com>
2025-02-08 10:08:11 +0900

    Create general_log_format_all.sh
    General Log format all DMLs.

---
+++ general_log/general_log_format_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Provide General Log File
+if [ "$#" -ne 1 ]; then
+    echo "Missing General Log. Usage: $0 general_log.log" >&2
+    exit 1
+fi
+
+# Extract all queries in order and clean up formatting
+grep -i "Query\s\+" $1 | \
+sed -n 's/.*Query\s\+\(SELECT\|INSERT\|UPDATE\|DELETE\).*$/\0/ip' | \
+sed -n 's/.*Query\s\+\(.*\)/\1/p' | \
+sed 's/[[:space:]]\+/ /g' | \
+# Put semi-colon if not.
+sed 's/$/;/' > all_queries.sql
+
+# Clean up the file
+sed -i -e '/^$/d' -e 's/"//g' all_queries.sql
